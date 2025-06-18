e0fb22c6351c50672480fd956e6223be0cb07bea
Christina Lin <weimeilin@gmail.com>
2025-06-17 20:12:08 -0400

    Update a2a_server.py

--- agents/planner/a2a_server.py
+++ agents/planner/a2a_server.py
@@ -10,9 +10,10 @@ from google.adk.memory.in_memory_memory_service import InMemoryMemoryService
 import os
 import logging
 from dotenv import load_dotenv
-from platform_mcp_client.agent_executor import PlatformAgentExecutor
+from planner.agent_executor import PlannerAgentExecutor
 import uvicorn
-from platform_mcp_client import agent
+from planner import agent
+
 
 load_dotenv()
 
@@ -20,74 +21,75 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 host=os.environ.get("A2A_HOST", "localhost")
-port=int(os.environ.get("A2A_PORT",10002))
+port=int(os.environ.get("A2A_PORT",10003))
 PUBLIC_URL=os.environ.get("PUBLIC_URL")
 
-class PlatformAgent:
-  """An agent that post event and post to instavibe."""
+class PlannerAgent:
+    """An agent to help user planning a event with its desire location."""
+    SUPPORTED_CONTENT_TYPES = ["text", "text/plain"]
 
-  SUPPORTED_CONTENT_TYPES = ["text", "text/plain"]
-
-  def __init__(self):
-    self._agent = self._build_agent()
-    self.runner = Runner(
-        app_name=self._agent.name,
-        agent=self._agent,
-        artifact_service=InMemoryArtifactService(),
-        session_service=InMemorySessionService(),
-        memory_service=InMemoryMemoryService(),
-    )
-    capabilities = AgentCapabilities(streaming=True)
-    skill = AgentSkill(
-            id="instavibe_posting",
-            name="Post social post and events on instavibe",
+    def __init__(self):
+        self._agent = self._build_agent()
+        self.runner = Runner(
+            app_name=self._agent.name,
+            agent=self._agent,
+            artifact_service=InMemoryArtifactService(),
+            session_service=InMemorySessionService(),
+            memory_service=InMemoryMemoryService(),
+        )
+        capabilities = AgentCapabilities(streaming=True)
+        skill = AgentSkill(
+            id="event_planner",
+            name="Event planner",
             description="""
-            This "Instavibe" agent helps you create posts (identifying author, text, and sentiment – inferred if unspecified) and register
-            for events (gathering name, date, attendee). It efficiently collects required information and utilizes dedicated tools
-            to perform these actions on your behalf, ensuring a smooth sharing experience.
+            This agent generates multiple fun plan suggestions tailored to your specified location, dates, and interests,
+            all designed for a moderate budget. It delivers detailed itineraries,
+            including precise venue information (name, latitude, longitude, and description), in a structured JSON format.
             """,
             tags=["instavibe"],
-            examples=["Create a post for me, the post is about my cute cat and make it positive, and I'm Alice"],
+            examples=["What about Bostona MA this weekend?"],
         )
-    self.agent_card = AgentCard(
-            name="Instavibe Posting Agent",
+        self.agent_card = AgentCard(
+            name="Event Planner Agent",
             description="""
-            This "Instavibe" agent helps you create posts (identifying author, text, and sentiment – inferred if unspecified) and register
-            for events (gathering name, date, attendee). It efficiently collects required information and utilizes dedicated tools
-            to perform these actions on your behalf, ensuring a smooth sharing experience.
+            This agent generates multiple fun plan suggestions tailored to your specified location, dates, and interests,
+            all designed for a moderate budget. It delivers detailed itineraries,
+            including precise venue information (name, latitude, longitude, and description), in a structured JSON format.
             """,
             url=f"{PUBLIC_URL}",
             version="1.0.0",
-            defaultInputModes=PlatformAgent.SUPPORTED_CONTENT_TYPES,
-            defaultOutputModes=PlatformAgent.SUPPORTED_CONTENT_TYPES,
+            defaultInputModes=PlannerAgent.SUPPORTED_CONTENT_TYPES,
+            defaultOutputModes=PlannerAgent.SUPPORTED_CONTENT_TYPES,
             capabilities=capabilities,
             skills=[skill],
         )
 
+    def get_processing_message(self) -> str:
+        return "Processing the planning request..."
 
-  def get_processing_message(self) -> str:
-      return "Processing the social post and event request..."
-
-  def _build_agent(self) -> LlmAgent:
-    """Builds the LLM agent for the Processing the social post and event request."""
-    return agent.root_agent
+    def _build_agent(self) -> LlmAgent:
+        """Builds the LLM agent for the night out planning agent."""
+        return agent.root_agent
 
 
 if __name__ == '__main__':
     try:
-        platformAgent = PlatformAgent()
+        plannerAgent = PlannerAgent()
 
         request_handler = DefaultRequestHandler(
-            agent_executor=PlatformAgentExecutor(platformAgent.runner,platformAgent.agent_card),
+            agent_executor=PlannerAgentExecutor(plannerAgent.runner,plannerAgent.agent_card),
             task_store=InMemoryTaskStore(),
         )
 
         server = A2AStarletteApplication(
-            agent_card=platformAgent.agent_card,
+            agent_card=plannerAgent.agent_card,
             http_handler=request_handler,
         )
+        logger.info(f"Attempting to start server with Agent Card: {plannerAgent.agent_card.name}")
+        logger.info(f"Server object created: {server}")
 
         uvicorn.run(server.build(), host='0.0.0.0', port=port)
     except Exception as e:
         logger.error(f"An error occurred during server startup: {e}")
         exit(1)
+
